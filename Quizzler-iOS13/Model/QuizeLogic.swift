bdd69e264eb84153963a4409138edd26a331d296
Shreyash <shreyashp47@gmail.com>
2023-12-13 22:54:01 +0530

    Multiple options type question answer

--- Quizzler-iOS13/Model/QuizeLogic.swift
+++ Quizzler-iOS13/Model/QuizeLogic.swift
@@ -11,18 +11,16 @@ import Foundation
 struct QuizeLogic{
     
     let quiz = [
-        QuestionStruct(q: "A slug's blood is green.", a: "True"),
-              QuestionStruct(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-              QuestionStruct(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-              QuestionStruct(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-              QuestionStruct(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-              QuestionStruct(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-              QuestionStruct(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-              QuestionStruct(q: "Google was originally called 'Backrub'.", a: "True"),
-              QuestionStruct(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-              QuestionStruct(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-              QuestionStruct(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-              QuestionStruct(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+        QuestionStruct(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correctAnswer: "Skin"),
+               QuestionStruct(q: "Five dollars is worth how many nickels?", a: ["25", "50", "100"], correctAnswer: "100"),
+               QuestionStruct(q: "What do the letters in the GMT time zone stand for?", a: ["Global Meridian Time", "Greenwich Mean Time", "General Median Time"], correctAnswer: "Greenwich Mean Time"),
+               QuestionStruct(q: "What is the French word for 'hat'?", a: ["Chapeau", "Écharpe", "Bonnet"], correctAnswer: "Chapeau"),
+               QuestionStruct(q: "In past times, what would a gentleman keep in his fob pocket?", a: ["Notebook", "Handkerchief", "Watch"], correctAnswer: "Watch"),
+               QuestionStruct(q: "How would one say goodbye in Spanish?", a: ["Au Revoir", "Adiós", "Salir"], correctAnswer: "Adiós"),
+               QuestionStruct(q: "Which of these colours is NOT featured in the logo for Google?", a: ["Green", "Orange", "Blue"], correctAnswer: "Orange"),
+               QuestionStruct(q: "What alcoholic drink is made from molasses?", a: ["Rum", "Whisky", "Gin"], correctAnswer: "Rum"),
+               QuestionStruct(q: "What type of animal was Harambe?", a: ["Panda", "Gorilla", "Crocodile"], correctAnswer: "Gorilla"),
+               QuestionStruct(q: "Where is Tasmania located?", a: ["Indonesia", "Australia", "Scotland"], correctAnswer: "Australia")
     ]
     
     var questionNumber = 0
@@ -30,7 +28,7 @@ struct QuizeLogic{
     
     
    mutating func checkAnswer(_ userAnswer:String) -> Bool{
-        if userAnswer == quiz[questionNumber].answer {
+        if userAnswer == quiz[questionNumber].correctAnswer{
             print("Right")
             userScore += 1
             return true
@@ -40,9 +38,15 @@ struct QuizeLogic{
         }
         
     }
+    
     func getQuestionText () -> String {
         return quiz[questionNumber].text
     }
+      func getQuestionOptionsText () -> [String] {
+          return quiz[questionNumber].answer
+    }
+    
+    
     func getProgress ()-> Float{
         
         return Float(questionNumber + 1) / Float(quiz.count)
